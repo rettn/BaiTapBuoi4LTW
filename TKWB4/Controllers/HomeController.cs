8fde13265759724dbf54e85d1486129ae2c198fc
rettn <trongtan20061224@gmail.com>
2026-09-12 22:46:53 +0700

    Commit Final Last :(

---
+++ TKWB4/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TKWB4.Models;
+
+namespace TKWB4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
---
+++ TKWB4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TKWB4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
